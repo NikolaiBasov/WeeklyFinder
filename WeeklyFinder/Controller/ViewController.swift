1f7da2a90d7b81fce9e4a9814facc3f5e39e1eaa
Nikolai_Basov <basov11nikolay@gmail.com>
2021-04-22 15:15:36 +0300

    Add group MVC

--- WeeklyFinder/Controller/ViewController.swift
+++ WeeklyFinder/Controller/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - IBOutlet
+
     @IBOutlet weak var dateTF: UITextField!
     @IBOutlet weak var monthTF: UITextField!
     @IBOutlet weak var yearTF: UITextField!
     @IBOutlet weak var resultLable: UILabel!
+    @IBOutlet weak var findDayButton: UIButton!
+
+    //MARK: - View Lifecycle
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+           findDayButton.layer.cornerRadius = 10
+    }
+
+    //MARK: - IBAction
+
     @IBAction func findDayTap(_ sender: UIButton) {
         sender.findDay()
         guard let day = dateTF.text,
@@ -24,16 +36,17 @@ class ViewController: UIViewController {
         dateComponents.day = Int(day)
         dateComponents.month = Int(month)
         dateComponents.year = Int(year)
-        
+    
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "ru_Ru")
         dateFormatter.dateFormat = "EEEE"
-        
+
         guard let date = calendar.date(from: dateComponents) else { return }
         let weekday = dateFormatter.string(from: date)
         let capitalazedWeekday = weekday.capitalized
         resultLable.text = capitalazedWeekday
     }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
